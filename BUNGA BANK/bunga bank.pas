b5a243e0691824203292fcfdbd6345ddaf258c3a
akmalikhsan <63221748+AkmalFounder@users.noreply.github.com>
2021-06-04 23:25:09 +0700

    Bunga Bank fix

---
+++ BUNGA BANK/bunga bank.pas
@@ -0,0 +1,22 @@
+{
+    $Id: gplprog.pt,v 1.2 2002/09/07 15:40:47 peter Exp 2018/12/19 17:28:28 peter Exp $
+    This file is part of
+    Copyright (c) 2018 by
+
+
+
+    See the file COPYING.FPC, included in this distribution,
+    for details about the copyright.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+ **********************************************************************}
+program ;
+
+uses ;
+
+BEGIN
+END.
+
