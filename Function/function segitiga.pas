6996cdbdd92cdb9527c380fa72c8773f292eac65
akmalikhsan <63221748+AkmalFounder@users.noreply.github.com>
2021-06-25 23:48:57 +0700

    Function

---
+++ Function/function segitiga.pas
@@ -0,0 +1,22 @@
+program segitiga;
+uses crt;
+function segitiga (a,b,c:integer)       :integer;
+begin
+        if (s1=s2)and(s1=s3)and(s2=s3) then
+                writeln('segitiga sama sisi');
+        else
+                (s1=s2)or(s2=s3)or(s1=s3);
+                writeln('segitiga sama kaki
+var
+        s1,s2,s3        :integer;
+begin
+        clrscr;
+        writeln('masukan sisi 1: ');
+        readln(s1);
+        writeln('masukan sisi 2: ');
+        readln(s2);
+        writeln('masukan sisi 3: ');
+        readln(s3);
+        writeln('segitiganya adalah: ',segitiga(s1,s2,s3);
+        readln;
+end.
