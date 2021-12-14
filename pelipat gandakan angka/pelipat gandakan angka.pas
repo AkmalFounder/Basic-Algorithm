7fe48c3b7a1baaeea1d62963f3ec2a8fa112f5c4
akmalikhsan <63221748+AkmalFounder@users.noreply.github.com>
2021-12-14 23:56:28 +0700

    gandakan angka

---
+++ pelipat gandakan angka/pelipat gandakan angka.pas
@@ -0,0 +1,13 @@
+program pelipatgandakanangka;
+uses crt;
+var
+        a,h     : real;
+begin
+        clrscr;
+        write('Masukan Angka :');
+        readln(a);
+        clrscr;
+        h:=(a * 2);
+        write('Hasilnya :', h:6:1);
+        readln;
+        end.
